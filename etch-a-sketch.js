aee542d4de81fc1bad6a7d0e308957424a6a35bf
Andrew Barnard <andrewbarnard09@gmail.com>
2023-07-30 10:53:36 -0500

    added click event listener for color button updates a colorOn variable by 1 which toggles if the square div background color will be black or random rgb values created random number generator function (based off of MDN website events tutorial)

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,6 +6,15 @@ const box= document.querySelector('.box');
 let squareWidth=0;
 
 const resetButton=document.querySelector("#resetButton");
+const colorButton=document.querySelector('#colorButton');
+let colorOn=1;
+
+//randomnumber generator function to create random colors for box when
+//colorOn varaible is even 
+function randomNumber(number){
+    return Math.floor(Math.random()* (number+1));
+}
+
 
 function createSketchBoxes(){
     squareWidth=(500/Math.sqrt(startingGrid));
@@ -21,31 +30,66 @@ function createSketchBoxes(){
         square.setAttribute('class','box');
         //add div 'square' to div 'screen'
         screen.appendChild(square);
-        //add event listener that changes background color to black for mouseover
-        square.onmouseover=()=>{
-            square.style.backgroundColor='black';
-            //square.style.border='1px solid green';
-        }       
+        //if colorOn variable is even
+        if (colorOn%2===0){
+            square.onmouseover=()=>{
+                //creates random color back ground color for square div
+                const randomColor= `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`;
+                square.style.backgroundColor=randomColor;
+            }
+        }
+        else{
+            //if colorOn is odd creates black background color of square div on mouseover event
+            square.onmouseover=()=>{
+                square.style.backgroundColor='black';
+            } 
+        }             
     }
-}
+}   
 createSketchBoxes();
+//add event listener to reset background color and create prompt
 resetButton.onclick=()=>{
     i=0
     while (screen.firstChild){
         screen.removeChild(screen.firstChild);
     }
+    //in prompt ask for size of sketch from 1-100
     startingGrid=Math.pow(parseInt(window.prompt('Enter size of Sketch. 1 is biggest sketch size 100 is the smallest')), 2);
+    console.log(startingGrid)
+    //check prompt input to make sure it falls in parameters
+    if (startingGrid%1!=0){
+        alert(`${startingGrid}NOT A NUMBER!!!!!!!`);
+        startingGrid=Math.pow(parseInt(window.prompt('Enter a number. 1 is biggest sketch size 100 is the smallest')), 2);
+    }
+    if( Math.sqrt(startingGrid)>125){
+        startingGrid=Math.pow(parseInt(window.prompt('Your number is too big. 1 is biggest sketch size 100 is the smallest')), 2);
+    }
+    else{
+        alert("Draw to your heart's content");
+    }
     createSketchBoxes();
 }
-//add event listener to reset background color and create prompt
+
+colorButton.onclick=()=>{
+    //clears screen 
+    while (screen.firstChild){
+        screen.removeChild(screen.firstChild);
+    }
+    //toggles 1 color or black or white
+    colorOn+=1;
+    //lets user know if they are turning on the color mode or not
+    if (colorOn%2===0){
+        alert("Let the color flow!!!!!!!")
+    }
+    else{
+        alert("Classic Black and White"); 
+    }
+    createSketchBoxes();
+}
+    
 
 
 
 
-//in prompt ask for size of sketch from 1-100
-//check prompt input to make sure it falls in parameters
-//from prompt number determine # of divs by squaring the prompted number (ie 6=36 divs)
-
-//update box style width and height to 500/prompt#
 
 
