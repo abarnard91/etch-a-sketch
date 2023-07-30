9041d107e7475b70e480c3faf2d43f13809e79d1
Andrew Barnard <andrewbarnard09@gmail.com>
2023-07-30 08:51:47 -0500

    in js file moved for loop for adding square divs to the screen to a function to be called at the beginning of the program and to the reset button. added reset button click event to create prompt to change box size. removed green border on boxes b/c it was causing strange flexboxing with box sizes bigger than 30. removing border fixed the problem in css file changed buttons to have rounded corners

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,30 +3,49 @@ const screen=document.querySelector('#screen');
 let startingGrid=16;
 
 const box= document.querySelector('.box');
-let squareWidth=(500/Math.sqrt(startingGrid)-2);
+let squareWidth=0;
 
-for (let i=0;i<startingGrid; i++){
-    const square=document.createElement('div');
-    square.id="box"+i+1;
-    square.style.width=squareWidth +"px";
-    square.style.height=squareWidth+'px';
-    square.setAttribute('class','box');
-    screen.appendChild(square);
-    //add event listener that changes background color to black for mouseover
-    square.onmouseover=()=>{
-        square.style.backgroundColor='black';
-        square.style.border='1px solid green';
+const resetButton=document.querySelector("#resetButton");
+
+function createSketchBoxes(){
+    squareWidth=(500/Math.sqrt(startingGrid));
+    for (let i=0;i<startingGrid; i++){
+        //create div 
+        const square=document.createElement('div');
+        //add id box01 to boxi (whatever gridnumber is)
+        square.id="box"+i+1;
+        //create box dimensions (height & width should be ==)
+        square.style.width=squareWidth +"px";
+        square.style.height=squareWidth+'px';
+        //add class box to it
+        square.setAttribute('class','box');
+        //add div 'square' to div 'screen'
+        screen.appendChild(square);
+        //add event listener that changes background color to black for mouseover
+        square.onmouseover=()=>{
+            square.style.backgroundColor='black';
+            //square.style.border='1px solid green';
+        }       
     }
 }
-
-
-
-
+createSketchBoxes();
+resetButton.onclick=()=>{
+    i=0
+    while (screen.firstChild){
+        screen.removeChild(screen.firstChild);
+    }
+    startingGrid=Math.pow(parseInt(window.prompt('Enter size of Sketch. 1 is biggest sketch size 100 is the smallest')), 2);
+    createSketchBoxes();
+}
 //add event listener to reset background color and create prompt
-//in prompt ask for size of sketch from 1-10 
+
+
+
+
+//in prompt ask for size of sketch from 1-100
 //check prompt input to make sure it falls in parameters
 //from prompt number determine # of divs by squaring the prompted number (ie 6=36 divs)
-//and create and add div to screen parent and add class box to it
+
 //update box style width and height to 500/prompt#
 
 
